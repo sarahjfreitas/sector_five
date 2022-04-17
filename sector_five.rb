51f42f06aba431222489959304af4e00a280eaa4
Sarah Freitas <sarahjfreitas@gmail.com>
2022-04-17 10:35:54 -0300

    add multiples enemies

--- sector_five.rb
+++ sector_five.rb
@@ -6,25 +6,37 @@ require_relative 'enemy'
 class SectorFive < Gosu::Window
   WIDTH = 800
   HEIGHT = 600
+  ENEMY_FREQUENCY = 0.05
 
   def initialize
     super(800,600)
     self.caption = 'Sector Five'
     @player = Player.new(self)
-    @enemy = Enemy.new(self)
+    @enemies = []
   end
 
   def update
-    @player.turn_left if button_down?(Gosu::KB_LEFT) or button_down?(Gosu::GP_LEFT)
-    @player.turn_right if button_down?(Gosu::KB_RIGHT) or button_down?(Gosu::GP_RIGHT)
-    @player.acelerate if button_down?(Gosu::KB_UP) or button_down?(Gosu::GP_BUTTON_0)
-    @player.move
-    @enemy.move
+    move_player
+    create_enemy
+    @enemies.each {|enemy| enemy.move }
   end
 
   def draw
     @player.draw
-    @enemy.draw
+    @enemies.each {|enemy| enemy.draw }
+  end
+
+  private
+
+  def move_player
+    @player.turn_left if button_down?(Gosu::KB_LEFT) or button_down?(Gosu::GP_LEFT)
+    @player.turn_right if button_down?(Gosu::KB_RIGHT) or button_down?(Gosu::GP_RIGHT)
+    @player.acelerate if button_down?(Gosu::KB_UP) or button_down?(Gosu::GP_BUTTON_0)
+    @player.move
+  end
+
+  def create_enemy
+    @enemies.push Enemy.new(self) if rand < ENEMY_FREQUENCY
   end
 end
 
